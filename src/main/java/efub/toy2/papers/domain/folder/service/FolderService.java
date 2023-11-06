9eebfb4735c6aed0006325b817ebd9f6901126c3
nammsamm <ahtnwl1004@gmail.com>
2023-11-06 21:45:15 +0900

    feat : S3 설정

--- src/main/java/efub/toy2/papers/domain/folder/service/FolderService.java
+++ src/main/java/efub/toy2/papers/domain/folder/service/FolderService.java
@@ -1,12 +1,21 @@
 package efub.toy2.papers.domain.folder.service;
 
 import efub.toy2.papers.domain.folder.domain.Folder;
+import efub.toy2.papers.domain.folder.dto.FolderRequestDto;
+import efub.toy2.papers.domain.folder.dto.FolderResponseDto;
 import efub.toy2.papers.domain.folder.repository.FolderRepository;
 import efub.toy2.papers.domain.member.domain.Member;
+import efub.toy2.papers.domain.scrap.domain.Scrap;
+import efub.toy2.papers.domain.scrap.repository.ScrapRepository;
+import efub.toy2.papers.global.exception.CustomException;
+import efub.toy2.papers.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -15,7 +24,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class FolderService {
 
     public final FolderRepository folderRepository;
+    private final ScrapRepository scrapRepository;
 
+    /* 기본 폴더 생성 */
     public Folder createDefaultFolder(Member member) {
         Folder folder = Folder.builder()
                 .folderName("default_folder")
@@ -24,5 +35,59 @@ public class FolderService {
         folderRepository.save(folder);
 
         return folder;
+    }
+
+    /* 폴더 생성 */
+    public FolderResponseDto createFolder(Member member, FolderRequestDto requestDto) {
+        Folder folder = Folder.builder()
+                .folderName(requestDto.getFolderName())
+                .folderOwner(member)
+                .build();
+        folderRepository.save(folder);
+        return new FolderResponseDto(folder);
+    }
+
+    /* 폴더 삭제 */
+    public String deleteFolder(Member member, Long folderId) {
+        Folder folder = findFolderByFolderId(folderId);
+        if(!isFolderOwner(member,folder)) throw new CustomException(ErrorCode.INVALID_MEMBER);
+        if(folder.getFolderName().equals("default_folder")) throw new CustomException(ErrorCode.DEFAUT_FOLDER_CANNOT_DELETE);
+        folderRepository.delete(folder);
+        return "폴더가 삭제되었습니다.";
+    }
+
+    /* 폴더 id 로 폴더 조회 */
+    public Folder findFolderByFolderId(Long folderId){
+        Folder folder = folderRepository.findById(folderId)
+                .orElseThrow(()->new CustomException(ErrorCode.NO_FOLDER_EXIST));
+        return folder;
+    }
+
+    /* 폴더의 스크랩 목록 조회 */
+    public List<FolderResponseDto> findScrapListByFolderId(Member member, Long folderId) {
+        Folder folder = findFolderByFolderId(folderId);
+        List<Scrap> scrapList = scrapRepository.findAllByFolderOrderByCreatedAtDesc(folder);
+        List<FolderResponseDto> responseDtoList = new ArrayList<>();
+        for(Scrap scrap : scrapList){
+            responseDtoList.add(new FolderResponseDto(folder));
+        }
+        return responseDtoList;
+    }
+
+    /* 폴더의 이름 변경 */
+    public FolderResponseDto updateFolderName(Member member, Long folderId , FolderRequestDto requestDto) {
+        Folder folder = findFolderByFolderId(folderId);
+        if(!isFolderOwner(member,folder)) throw new CustomException(ErrorCode.INVALID_MEMBER);
+        folder.updateFolderName(requestDto.getFolderName());
+        return new FolderResponseDto(folder);
+    }
+
+
+    /* 회원이 폴더의 주인인지 검사 */
+    public Boolean isFolderOwner(Member member , Folder folder){
+        Boolean isOwner;
+        if(folder.getFolderOwner().getMemberId() == member.getMemberId()) isOwner = true;
+        else isOwner = false;
+        return isOwner;
     }
 }
