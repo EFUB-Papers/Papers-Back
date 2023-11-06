9d600a192cfdc542c9072266e622bc40e4f9beeb
nammsamm <ahtnwl1004@gmail.com>
2023-11-06 13:38:36 +0900

    fix : 댓글 응답으로 작성 시간, 작성자 확인 추가

--- src/main/java/efub/toy2/papers/domain/comment/repository/CommentRepository.java
+++ src/main/java/efub/toy2/papers/domain/comment/repository/CommentRepository.java
@@ -4,4 +4,5 @@ import efub.toy2.papers.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment , Long> {
+
 }
