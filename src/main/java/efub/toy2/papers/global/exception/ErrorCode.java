8d9e7697ee643236462d636a3fe2441cf48f6535
nammsamm <ahtnwl1004@gmail.com>
2023-11-23 23:46:03 +0900

    fix : 401 에러로 수정

--- src/main/java/efub/toy2/papers/global/exception/ErrorCode.java
+++ src/main/java/efub/toy2/papers/global/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
     NO_MEMBER_EXIST(HttpStatus.BAD_REQUEST , "가입되지 않은 회원입니다."),
     ALREADY_LIKED(HttpStatus.BAD_REQUEST,"이미 좋아요를 누른 스크랩입니다."),
     EXPIRED_TOKEN(HttpStatus.BAD_REQUEST,"만료된 토큰입니다." ),
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST,"유효하지 않은 토큰입니다." ),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED,"유효하지 않은 토큰입니다." ),
     NON_LOGIN(HttpStatus.BAD_REQUEST,"로그인이 필요합니다." ),
     INVALID_MEMBER(HttpStatus.BAD_REQUEST,"접근 권한이 없는 회원입니다."),
     ALREADY_EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다."),
