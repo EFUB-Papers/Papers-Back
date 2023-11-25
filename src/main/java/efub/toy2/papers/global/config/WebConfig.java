7df5f7befb0475c5b68884fd934ac644ff0acb40
Haeun Noh <00blowup@gmail.com>
2023-11-26 03:49:49 +0900

    fix: CORS 설정

--- src/main/java/efub/toy2/papers/global/config/WebConfig.java
+++ src/main/java/efub/toy2/papers/global/config/WebConfig.java
@@ -2,7 +2,9 @@ package efub.toy2.papers.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,4 +18,22 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolverList){
         argumentResolverList.add(authUserArgumentResolver);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("")
+                .allowedOrigins("")
+                .allowedMethods(HttpMethod.GET.name())
+                .allowedMethods(HttpMethod.POST.name())
+                .allowedMethods(HttpMethod.PUT.name())
+                .allowedMethods(HttpMethod.DELETE.name())
+                .allowedMethods(HttpMethod.PATCH.name())
+                .allowedHeaders("Authorization")
+                .allowedHeaders("refresh-token")
+                .allowCredentials(true);
+    }
+
+
 }
