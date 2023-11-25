2cdbb0dbe82f8b63c854fed1e54221bf0d1be7a5
Haeun Noh <00blowup@gmail.com>
2023-11-26 04:09:18 +0900

    fix: CORS 설정

--- src/main/java/efub/toy2/papers/global/config/WebConfig.java
+++ src/main/java/efub/toy2/papers/global/config/WebConfig.java
@@ -19,21 +19,4 @@ public class WebConfig implements WebMvcConfigurer {
         argumentResolverList.add(authUserArgumentResolver);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("")
-                .allowedOrigins("")
-                .allowedMethods(HttpMethod.GET.name())
-                .allowedMethods(HttpMethod.POST.name())
-                .allowedMethods(HttpMethod.PUT.name())
-                .allowedMethods(HttpMethod.DELETE.name())
-                .allowedMethods(HttpMethod.PATCH.name())
-                .allowedHeaders("Authorization")
-                .allowedHeaders("refresh-token")
-                .allowCredentials(true);
-    }
-
-
 }
