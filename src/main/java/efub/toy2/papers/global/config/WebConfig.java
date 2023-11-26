2f76a975c0ed588e7559dae1584e720d5f05aa3a
Haeun Noh <00blowup@gmail.com>
2023-11-26 16:24:50 +0900

    refactor: 필요없는 import문 삭제

--- src/main/java/efub/toy2/papers/global/config/WebConfig.java
+++ src/main/java/efub/toy2/papers/global/config/WebConfig.java
@@ -2,9 +2,7 @@ package efub.toy2.papers.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
