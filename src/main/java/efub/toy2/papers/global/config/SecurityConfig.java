385ec84ff89f69088764ed18563876992a551fda
Haeun Noh <00blowup@gmail.com>
2023-11-26 18:58:55 +0900

    refactor: 필요 없는 import 문 삭제

--- src/main/java/efub/toy2/papers/global/config/SecurityConfig.java
+++ src/main/java/efub/toy2/papers/global/config/SecurityConfig.java
@@ -4,12 +4,9 @@ import efub.toy2.papers.domain.member.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
