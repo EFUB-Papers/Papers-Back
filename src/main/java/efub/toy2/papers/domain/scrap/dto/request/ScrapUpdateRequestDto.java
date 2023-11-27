27d805656bf22b0ee360e07a6ed65944d44e17f1
Haeun Noh <00blowup@gmail.com>
2023-11-27 18:08:44 +0900

    fix: 스크랩 수정 DTO의 필드들에 Nullable 어노테이션 추가

--- src/main/java/efub/toy2/papers/domain/scrap/dto/request/ScrapUpdateRequestDto.java
+++ src/main/java/efub/toy2/papers/domain/scrap/dto/request/ScrapUpdateRequestDto.java
@@ -3,6 +3,7 @@ package efub.toy2.papers.domain.scrap.dto.request;
 import efub.toy2.papers.domain.tag.dto.request.TagWriteRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import reactor.util.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,10 +11,16 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class ScrapUpdateRequestDto {
+    @Nullable
     private String scrapTitle;
+    @Nullable
     private String scrapLink;
+    @Nullable
     private String scrapContent;
+    @Nullable
     private String category;
+    @Nullable
     private Long folderId;
+    @Nullable
     private final List<TagWriteRequestDto> tags = new ArrayList<>();
 }
